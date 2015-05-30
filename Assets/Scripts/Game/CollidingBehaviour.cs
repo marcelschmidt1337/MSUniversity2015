5b06566fc218f6befd3c0b9d4539ae2ea87852e2
unknown <s0538289@htw-berlin.de>
2015-05-31 01:51:43 +0200

    some more gamelogic

---
+++ Assets/Scripts/Game/CollidingBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollidingBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnCollisionEnter(Collision collision) {
+        Weapon weapon = collision.collider.GetComponent<Weapon>();
+
+        if (weapon != null) {
+            Player weaponOwner = weapon.Owner;
+
+            if (weaponOwner != null) {
+
+                Car car = this.GetComponent<Car>();
+
+                GameManager.Instance.WeaponCrashesCar(weaponOwner.ID, car.Owner.ID, weapon);
+            }
+        }
+    }
+}
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -15,8 +15,10 @@ public class GameManager : MonoBehaviour {
     [SerializeField]
     Player playerprefab;
 
-    //[SerializeField]
-    //Car carPrefab
+    [SerializeField]
+    GameObject carPrefab;
+
+    HighScoreLogic highscorelogic;
 
     private Player[] player;
 
@@ -39,28 +41,36 @@ public class GameManager : MonoBehaviour {
     }
 
     public void WeaponCrashesCar(int origin, int victim, Weapon weapon) {
-
+        player[victim].addLivePoints(-crashDamagePoints);
     }
 
     public void RagdollFlies(int origin, int victim) {
-
+        player[victim].addLivePoints(-crashDamagePoints);
     }
 
     public void StartGame(int playerCount) {
-        PlayerCount = playerCount;
 
+        highscorelogic = new HighScoreLogic();
+
+        PlayerCount = playerCount;
+        player = new Player[playercount];
 
         for (int i = 0; i < playercount; i++) {
-            player[i] = GameObject.Instantiate<Player>(playerprefab);
-
-            Player playerData = player[i].GetComponent<Player>();
-            
-            int spawnPointIndex = Random.Range(0, spawnPoints.Length - 1);
-            //spawnPoints[spawnPointIndex].
-
-
-
+            SpawnPlayer(i);
         } 
+    }
+
+    public void SpawnPlayer(int id) {
+        player[id] = GameObject.Instantiate<Player>(playerprefab);
+
+        Player playerData = player[id].GetComponent<Player>();
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length - 1);
+
+        GameObject playerVehicle = GameObject.Instantiate<GameObject>(carPrefab);
+
+        playerVehicle.transform.position = spawnPoints[spawnPointIndex].transform.position;
+
     }
  
 
