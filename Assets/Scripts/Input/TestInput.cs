e99ab2f02f2000e003774bdbca83e1f91f4ce947
Julian Löhr <julian.loehr@outlook.com>
2015-05-30 18:28:58 +0200

    Added Standart Vehicle Car Assets, Test Scene and a Car Prefab with Test Input for Keyboard

---
+++ Assets/Scripts/Input/TestInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+
+[RequireComponent(typeof(CarController))]
+public class TestInput : MonoBehaviour 
+{
+    private CarController m_Car; // the car controller we want to use
+
+    private void Awake()
+    {
+        Debug.Log("hi");
+        // get the car controller
+        m_Car = GetComponent<CarController>();
+    }
+
+    private void FixedUpdate()
+    {
+        // pass the input to the car!
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float handbrake = Input.GetAxis("Jump");
+        m_Car.Move(h, v, v, handbrake);
+    }
+      
+}
