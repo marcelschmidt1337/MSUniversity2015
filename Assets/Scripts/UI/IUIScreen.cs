21a9ef1e06bc701e090eff70635c675317bdaf08
Marcel Schmidt <marcelschmidt1337@gmail.com>
2015-05-30 19:20:16 +0200

    Started working on UI system

---
+++ Assets/Scripts/UI/IUIScreen.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IUIScreen
+{
+	UIState StateId {get;}
+	void Activate();
+	void Deactivate(System.Action onDone);
+}
---
+++ Assets/Scripts/UI/Screens/MainScreen.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainScreen : MonoBehaviour, IUIScreen
+{
+	public UIState StateId {get {return UIState.MainScreen; } }
+
+	public void Activate () {
+		this.gameObject.SetActive( true );
+	}
+
+	public void Deactivate (System.Action onDone) {
+		this.gameObject.SetActive( false );
+		onDone();
+	}
+
+	public void Quit () {
+#if UNITY_EDITOR
+		EditorApplication.isPlaying = false;
+#endif
+		Application.Quit();
+	}
+
+	public void Highscore () {
+		UIScreenHandler.Instance.ChangeState( UIState.HighscoreScreen );
+	}
+
+	public void StartGame () {
+		throw new System.NotImplementedException();
+	}
+}
