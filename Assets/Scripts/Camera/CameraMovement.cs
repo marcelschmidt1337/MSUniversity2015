eac7fb1f99a3d504e312d0dfd47bfde1c8692730
Julian Löhr <julian.loehr@outlook.com>
2015-05-31 14:19:23 +0200

    Added Camera Movement and changed Arena Lightning

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 PlayerMiddle = Vector3.zero;
+        Vector3 TopLeft = Vector3.zero;
+        Vector3 BottomRight = Vector3.zero;
+
+        GameObject[] Players = GameManager.Instance.spawnPoints;
+
+        foreach(GameObject Car in Players)
+        {
+            if(Car.activeInHierarchy)
+            {
+                if(Car.transform.position.x > TopLeft.x)
+                {
+                    TopLeft.x = Car.transform.position.x;
+                }
+
+                if (Car.transform.position.y < TopLeft.y)
+                {
+                    TopLeft.y = Car.transform.position.y;
+                }
+
+                if (Car.transform.position.x < BottomRight.x)
+                {
+                    BottomRight.x = Car.transform.position.x;
+                }
+
+                if (Car.transform.position.y > BottomRight.y)
+                {
+                    BottomRight.y = Car.transform.position.y;
+                }
+
+                PlayerMiddle += Car.transform.position / 2f ;
+            }
+        }
+
+        transform.position = new Vector3(PlayerMiddle.x, transform.position.y, PlayerMiddle.z);
+        Debug.Log(Vector3.Distance(TopLeft, BottomRight));
+	}
+}
