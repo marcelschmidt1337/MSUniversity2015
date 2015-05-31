b2656a98f1504cf031d385bf55b93c87eafa7621
unknown <s0538289@htw-berlin.de>
2015-05-31 12:18:52 +0200

    solved merge conflicts

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -88,8 +88,6 @@ public class GameManager : MonoBehaviour {
 
         int spawnPointIndex = Random.Range(0, spawnPoints.Length - 1);
 
-        playerVehicle.transform.position = spawnPoints[spawnPointIndex].transform.position;
-
         XInputControlled Controller = spawnPoints[SpawnPosition].GetComponent<XInputControlled>();
         Controller.PlayerIndex = (XInputDotNetPure.PlayerIndex)id;
     }
